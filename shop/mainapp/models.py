5aae9eb94947903f337cf1bf4d7e088ef614cc79
Sham-An <shamraev@inbox.ru>
2021-09-13 22:50:21 +0300

    https://youtu.be/jME4-T_hfhQ?t=4541 Создали товары, перешли в shell !!!!!

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -5,6 +5,22 @@ from  django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model() # settings.AUTH_USER_MODEL
 
+class LatestProductsManager:
+
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        products = []
+        ct_models = ContentType.objects.first(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+        return products
+
+
+class LatestProducts:
+
+    objects = LatestProductsManager()
+
 # Create your models here.
 #*******
 #Category
@@ -39,6 +55,33 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+class Notebook(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    processor_freq = models.CharField(max_length=255, verbose_name='Частота процессора')
+    ram =models.CharField(max_length=255, verbose_name='Оперативная память')
+    video = models.CharField(max_length=255, verbose_name='Видеокарта')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Время работы аккумулятора')
+
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+
+
+class Smartphone(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    resolution = models.CharField(max_length=255, verbose_name='Разрешение экрана')
+    accum_volume = models.CharField(max_length=255, verbose_name='Объем батареи')
+    ram =models.CharField(max_length=255, verbose_name='Оперативная память')
+    sd = models.BooleanField(default=True)
+    sd_volume_max = models.CharField(max_length=255, verbose_name='Максимальный объем памяти')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Главная камера')
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name='Фронтальная камера')
+
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+
 
 class CartProduct(models.Model):
 
